5789f23a4382de2fb9f55a72b01fbed8f0afce43
Daniel Boek <daniel.k.boek@gmail.com>
2026-08-21 21:08:11 +1000

    Minor cleanup

--- scripts/generic.js
+++ scripts/generic.js
@@ -3,13 +3,13 @@
 
 
 function randomInt(max, min = 0) {
-// Generates a random integer between min (inclusive) and max (exclusive)
-if (min != 0) {
-    const temp = min;
-    min = max;
-    max = temp;
-}
-return Math.floor(min) + Math.floor(Math.random() * (max - min))
+    // Generates a random integer between min (inclusive) and max (exclusive)
+    if (min != 0) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    return Math.floor(min) + Math.floor(Math.random() * (max - min))
 }
 
 function clamp(val, min, max) {
